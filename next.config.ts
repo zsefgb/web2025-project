3e3396aba4a44dee833aa4fac029296b24b4c4be
zsefgb <rlagmlcks777@gmail.com>
2025-05-25 20:01:53 +0900

    배포

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   /* config options here */
-  assetPrefix: process.env.NODE_ENV === "production" ? "web2025-project/" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://zsefgb.github.io/web2025-project/"
+      : "",
 };
 
 export default nextConfig;
